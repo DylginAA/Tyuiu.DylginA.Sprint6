984b1e5922d5ca3be360b9045b0e84671fc42ea0
Admin <Admin@HOME-PC>
2024-12-12 20:03:21 +0500

    Task6

---
+++ Tyuiu.DylginA.Sprint6.Task6.V10.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.DylginA.Sprint6.Task6.V10.Lib
+{
+    public class DataService : ISprint6Task6V10
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string result = "";
+
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    if (word.ToLower().Contains("w"))
+                    {
+                        result += word + " ";
+                    }
+                }
+            }
+            return result.Trim();
+        }
+    }
+}

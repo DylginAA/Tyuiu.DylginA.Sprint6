0e1e30feb83c744054926c34fd355155ba6cb793
Admin <Admin@HOME-PC>
2024-12-12 20:03:52 +0500

    Task6

---
+++ Tyuiu.DylginA.Sprint6.Task6.V10.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DylginA.Sprint6.Task6.V10.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
